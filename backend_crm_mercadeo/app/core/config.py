168a33e5322c073c23e5a3ed1c9ea2c3dc539400
JuanCespedes_LALIGA <practicante.sistemas@laliga.org.co>
2026-08-05 09:24:56 -0500

    prueba docker local

--- backend_crm_mercadeo/app/core/config.py
+++ backend_crm_mercadeo/app/core/config.py
@@ -23,6 +23,8 @@ class Settings(BaseSettings):
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:5175",
+        "http://127.0.0.1:5175",
         "http://160.2.1.80:3000",
         "http://160.2.1.80:5175",
     ]
